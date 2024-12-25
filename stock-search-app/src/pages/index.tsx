e90e69d42e39db84cecf908ea3a30cbdd7122a81
Pranav-20186017 <sharmapranav307@gmail.com>
2024-12-24 19:49:26 -0500

    added aws api endpoint to next js for vercel

--- stock-search-app/src/pages/index.tsx
+++ stock-search-app/src/pages/index.tsx
@@ -137,7 +137,7 @@ const HomePage: React.FC = () => {
         };
 
         try {
-            const response = await axios.post('http://127.0.0.1:8000/optimize/', dataToSend);
+            const response = await axios.post('https://vgb7u5iqyb.execute-api.us-east-2.amazonaws.com/optimize/', dataToSend);
             const result = response.data as PortfolioOptimizationResponse;
             setOptimizationResult(result);
         } catch (error) {
