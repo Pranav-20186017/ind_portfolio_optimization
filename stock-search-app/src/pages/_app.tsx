1e71cfd53649cc8ec1dc0861a92917d9af606c1a
Pranav-20186017 <sharmapranav307@gmail.com>
2025-04-14 00:49:33 -0400

    added vercel speed-insights

--- stock-search-app/src/pages/_app.tsx
+++ stock-search-app/src/pages/_app.tsx
@@ -3,7 +3,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react'; // Import Vercel Analytics
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
